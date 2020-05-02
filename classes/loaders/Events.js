241dd83269ae040a5c54840c01fc43db4c8f6cb5
root <vip.x49@mail.ru>
2020-05-02 13:58:06 +0300

    Лоадер ивентов

---
+++ classes/loaders/Events.js
@@ -0,0 +1,30 @@
+const Enmap = require('enmap');
+const fs = require('fs');
+
+class BotEvents extends Enmap {
+    constructor(args) {
+        super(args)
+
+        /**
+         * Папка с элементами.
+         */
+        this.dir = args.dir;
+        this.client = args.client;
+    }
+
+    /**
+     * Загрузка всех элементов
+     */
+    load() {
+        fs.readdirSync(this.dir)
+            .filter(event => event.endsWith(".js"))
+            .forEach((event, index) => {
+                const loadedEvent = require(`${this.dir}/${event}`);
+                let eventName = event.split(".")[0];
+                this.client.on(eventName, loadedEvent);
+                delete require.cache[require.resolve(`${this.dir}/${event}`)];
+            });
+    }
+} 
+
+module.exports = BotEvents;
